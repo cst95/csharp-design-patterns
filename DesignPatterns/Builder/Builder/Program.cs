299c505e559a8873ce71d7cbe4d6bee0ced2b1a3
Charlie Stokes <cstokes873@gmail.com>
2020-08-27 20:12:27 +0100

    Make builder project a console app and remove redundant design patterns project.

---
+++ DesignPatterns/Builder/Builder/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Builder;
+
+namespace DesignPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Fluent Builder Pattern
+            var eb = new EmployeeBuilder();
+
+            var person = eb
+                .WithName("Charlie")
+                .Earns(1000)
+                .WorksAs("Developer")
+                .Build();
+
+            Console.WriteLine(person);
+            
+            // Inherited Builder Pattern
+            var person2 = Employee.New
+                .WorksAs("Developer")
+                .WithName("Charlie")
+                .Earns(12000)
+                .Build();
+
+            Console.WriteLine(person2);
+            
+            // Functional Builder Pattern
+            var fb = new FunctionEmployeeBuilder();
+            var person3 = fb
+                .WithName("Charlie")
+                .WorksAs("Developer")
+                .Earns(1000)
+                .Build();
+
+            Console.WriteLine(person3);
+        }
+    }
+}
